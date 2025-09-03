913eee2232fdee5107e85fa5d74c0d0e02780189
Heosion <tldhs123e@gmail.com>
2025-09-03 21:12:44 +0900

    learn : CSS 동적 스타일링(버튼), List(목록) 데이터 동적 출력 - 버튼 클릭 시, 디자인 추가 - 반복되는 목록을 map()을 사용해 데이터가 동적으로 출력이 되도록 수정

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -36,29 +36,36 @@ export default function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               {[CORE_CONCEPTS[0].title]}
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
               {[CORE_CONCEPTS[1].title]}
             </TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
               {[CORE_CONCEPTS[2].title]}
             </TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
               {[CORE_CONCEPTS[1].title]}
             </TabButton>
           </menu>
--- 01-starting-project/src/data.js
+++ 01-starting-project/src/data.js
@@ -8,25 +8,25 @@ export const CORE_CONCEPTS = [
     image: componentsImg,
     title: "Components",
     description:
-      "The core UI building block - compose the user interface by combining multiple components.",
+      "UI를 구성하는 핵심적인 기본 단위입니다. 여러 컴포넌트를 조합하여 전체 사용자 인터페이스를 만들 수 있습니다.",
   },
   {
     image: jsxImg,
     title: "JSX",
     description:
-      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+      "실제로 렌더링될 마크업을 정의하기 위해, 동적인 값도 포함할 수 있는 HTML과 유사한 코드를 반환합니다.",
   },
   {
     image: propsImg,
     title: "Props",
     description:
-      "Make components configurable (and therefore reusable) by passing input data to them.",
+      "컴포넌트에 입력 데이터를 전달하여 설정이 가능하도록 만들고, 이를 통해 재사용성을 높여줍니다.",
   },
   {
     image: stateImg,
     title: "State",
     description:
-      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+      "React가 관리하는 데이터입니다. 이 데이터가 변경되면 컴포넌트가 리렌더링되고 UI가 업데이트됩니다.",
   },
 ];
 
