a50e6210c88467b9ee9c802539c2c5e0483c897c
Heosion <tldhs123e@gmail.com>
2025-09-05 17:52:20 +0900

    learn : 게임보드 li와 button 요소로 만들기

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
           <Player initialName="Player1" symbol="X" />
           <Player initialName="Player2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   );
