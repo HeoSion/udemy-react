ea5f4065bbda4ab9fe70251c8e43f07d4c82f1ec
Heosion <tldhs123e@gmail.com>
2025-09-09 17:36:04 +0900

    learn : 게임 재시작할 수 있게 배열을 복사하여 로직 작성

--- 02-tic-tac-toe-project/src/components/GameOver.jsx
+++ 02-tic-tac-toe-project/src/components/GameOver.jsx
@@ -1,11 +1,13 @@
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over</h2>
       {winner && <p>{winner} 승리!</p>}
       {!winner && <p>무승부!</p>}
       <p>
-        <button type="button">다시 시작!</button>
+        <button type="button" onClick={onRestart}>
+          다시 시작!
+        </button>
       </p>
     </div>
   );
